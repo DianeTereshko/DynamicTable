26918c55a2fa0ed79a38c6e1177c74858deaa8c0
DianeTereshko <45483517+DianeTereshko@users.noreply.github.com>
2019-08-31 11:10:14 +0500

    commit

--- DinamicTable/TypeScripts/tablesort.ts
+++ DinamicTable/TypeScripts/tablesort.ts
@@ -1,55 +1,63 @@
+
 interface IRow {
+    id: number;
     name: string;
     type: string;
     isvisble: boolean;
 }
 
 class Row implements IRow {
+    public id: number;
     public name: string;
     public type: string;
     public isvisble: boolean;
-    constructor(name: string, type: string, isvisible: boolean) {
+    constructor(id: number, name: string, type: string, isvisible: boolean) {
+        this.id = id;
         this.name = name;
         this.type = type;
         this.isvisble = isvisible;
     }
 }
 
-let head: Array<string> = ["Имя", "Тип", "Видимость"];
-let data1: IRow = new Row("Shannon Breitenberg", "Direct Tactics Producer", true);
-let data2: IRow = new Row("Haley Schiller", "Central Applications Supervisor", true);
-let data3: IRow = new Row("Sabrina Block II", "Legacy Functionality Officer", true);
-let data4: IRow = new Row("Maybell Volkman", "Product Web Facilitator", false);
-let data5: IRow = new Row("Zackery Paucek", "Regional Usability Liaison", true);
-let data6: IRow = new Row("Dr. Krystal Wilkinson", "Principal Tactics Planner", false);
-let data7: IRow = new Row("Chanelle McKenzie", "District Solutions Supervisor", true);
-let data8: IRow = new Row("Billy Kunze II", "тип строка", true);
-let data9: IRow = new Row("Eliezer Bailey", "Regional Accountability Executive", false);
-let data10: IRow = new Row("Rickie Fay", "Corporate Configuration Orchestrator", true);
-let data11: IRow = new Row("Kaycee Kub", "Chief Functionality Liaison", true);
-let data12: IRow = new Row("Destinee Mueller PhD", "Dynamic Identity Facilitator", false);
-let data13: IRow = new Row("Adeline Bins Sr.", "Regional Usability Associate", true);
-let data14: IRow = new Row("Myrtie Kunze", "Dynamic Intranet Supervisor", false);
-let data15: IRow = new Row("David Luettgen", "International Division Executive", true);
-let data16: IRow = new Row("Ismael Nolan", "International Metrics Assistant", false);
-let data17: IRow = new Row("Maegan Fay", "Principal Group Director", true);
-let data18: IRow = new Row("Zander Adams", "Principal Division Assistant", true);
-let data19: IRow = new Row("Onie Krajcik", "Chief Marketing Liaison", true);
-let data20: IRow = new Row("Helmer Huels", "Lead Interactions Analyst", true);
-let data21: IRow = new Row("Rhiannon Walker", "District Integration Developer", false);
-let data22: IRow = new Row("Dianna Nolan", "Lead Quality Producer", true);
-let data23: IRow = new Row("Garfield Upton", "National Quality Liaison", false);
-let data24: IRow = new Row("Xavier Stamm", "Customer Configuration Administrator", true);
-let data25: IRow = new Row("Gino Heller", "Product Tactics Supervisor", true);
-let data26: IRow = new Row("Beulah Mitchell", "Investor Tactics Administrator", false);
-let data27: IRow = new Row("Destany Johnston", "Forward Identity Director", true);
-let data28: IRow = new Row("Kamron VonRueden", "Central Solutions Assistant", true);
-let data29: IRow = new Row("Jaylen Nitzsche PhD", "Corporate Usability Administrator", false);
-let data30: IRow = new Row("Turner Lemke", "Regional Data Developer", true);
-let data31: IRow = new Row("Chris Schaefer", "Customer Security Officer", false);
-let data32: IRow = new Row("Dillon Upton", "Regional Factors Associate", true);
-let data33: IRow = new Row("Austen Marks", "Chief Response Consultant", false);
-let data34: IRow = new Row("Kenton Cartwright", "Customer Intranet Architect", true);
+interface IService {
+    DrowTable(head: Array<String>, data: Array<IRow>): void;
+}
+
+let head: Array<any> = ["Id", "Тип", "Видимость", "Активный"];
+let data1: IRow = new Row(1, "Shannon Breitenberg", "Direct Tactics Producer", true);
+let data2: IRow = new Row(2, "Haley Schiller", "Central Applications Supervisor", true);
+let data3: IRow = new Row(3, "Sabrina Block II", "Legacy Functionality Officer", true);
+let data4: IRow = new Row(4, "Maybell Volkman", "Product Web Facilitator", false);
+let data5: IRow = new Row(5, "Zackery Paucek", "Regional Usability Liaison", true);
+let data6: IRow = new Row(6, "Dr. Krystal Wilkinson", "Principal Tactics Planner", false);
+let data7: IRow = new Row(7, "Chanelle McKenzie", "District Solutions Supervisor", true);
+let data8: IRow = new Row(8, "Billy Kunze II", "Lead Quality Producer", true);
+let data9: IRow = new Row(9, "Eliezer Bailey", "Regional Accountability Executive", false);
+let data10: IRow = new Row(10, "Rickie Fay", "Corporate Configuration Orchestrator", true);
+let data11: IRow = new Row(11, "Kaycee Kub", "Chief Functionality Liaison", true);
+let data12: IRow = new Row(12, "Destinee Mueller PhD", "Dynamic Identity Facilitator", false);
+let data13: IRow = new Row(13, "Adeline Bins Sr.", "Regional Usability Associate", true);
+let data14: IRow = new Row(14, "Myrtie Kunze", "Dynamic Intranet Supervisor", false);
+let data15: IRow = new Row(15, "David Luettgen", "International Division Executive", true);
+let data16: IRow = new Row(16, "Ismael Nolan", "International Metrics Assistant", false);
+let data17: IRow = new Row(17, "Maegan Fay", "Principal Group Director", true);
+let data18: IRow = new Row(18, "Zander Adams", "Principal Division Assistant", true);
+let data19: IRow = new Row(19, "Onie Krajcik", "Chief Marketing Liaison", true);
+let data20: IRow = new Row(20, "Helmer Huels", "Lead Interactions Analyst", true);
+let data21: IRow = new Row(21, "Rhiannon Walker", "District Integration Developer", false);
+let data22: IRow = new Row(22, "Dianna Nolan", "Lead Quality Producer", true);
+let data23: IRow = new Row(23, "Garfield Upton", "National Quality Liaison", false);
+let data24: IRow = new Row(24, "Xavier Stamm", "Customer Configuration Administrator", true);
+let data25: IRow = new Row(25, "Gino Heller", "Product Tactics Supervisor", true);
+let data26: IRow = new Row(26, "Beulah Mitchell", "Investor Tactics Administrator", false);
+let data27: IRow = new Row(27, "Destany Johnston", "Forward Identity Director", true);
+let data28: IRow = new Row(28, "Kamron VonRueden", "Central Solutions Assistant", true);
+let data29: IRow = new Row(29, "Jaylen Nitzsche PhD", "Corporate Usability Administrator", false);
+let data30: IRow = new Row(30, "Turner Lemke", "Regional Data Developer", true);
+let data31: IRow = new Row(31, "Chris Schaefer", "Customer Security Officer", false);
+let data32: IRow = new Row(32, "Dillon Upton", "Regional Factors Associate", true);
+let data33: IRow = new Row(33, "Austen Marks", "Chief Response Consultant", false);
+let data34: IRow = new Row(34, "Kenton Cartwright", "Customer Intranet Architect", true);
 let data: Array<IRow> = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, data24, data25, data26, data27, data28, data29, data30, data31, data32, data33, data34]
 
 DrowTable(head, data);
@@ -57,12 +65,12 @@ DrowTable(head, data);
 
 function DrowTable(head: Array<String>, data: Array<IRow>) {
 
-    let theader = `<thead scope="col"><tr class="w3-red"><th><p>${head[1]}<i id="head-name" class="head-name w3-ext-cursor material-icons w3-ext-sort-icon">unfold_more</i></p></th><th><p>${head[1]}</p></th><th>${head[2]}</th><th>Действия</th></tr></thead>`;
+    let theader = `<thead scope="col"><tr class="w3-red"><th><p>${head[1]}<i id="head-name" class="head-name w3-ext-cursor material-icons w3-ext-sort-icon">unfold_more</i></p></th><th><p>${head[2]}</p></th><th>${head[3]}</th><th>Action</th></tr></thead>`;
     let tbody = `<tbody></tbody>`;
     $("table").append(theader);
     $("table").append(tbody);
     for (let row of data) {
-        let newrow = `<tr><td><p contenteditable id="name">${row.name}</p></td><td><p id="type" contenteditable>${row.type}</p></td><td><select><option value="${row.isvisble}">${row.isvisble}</option><option id=isvisible value="${!row.isvisble}">${!row.isvisble}</option></select></td><td><a href="#" onclick="SendToMVC(event)" class="w3-ext-button-animate w3-button w3-small">Сохранить</a></td></tr>`;
+        let newrow = `<tr><td><p contenteditable id="name">${row.name}</p></td><td><p id="type" contenteditable>${row.type}</p></td><td><select><option value="${row.isvisble}">${row.isvisble}</option><option id=isvisible value="${!row.isvisble}">${!row.isvisble}</option></select></td><td><a href="#" onclick="SendToASPCore(event)" class="w3-ext-button-animate w3-button w3-small">Сохранить</a></td></tr>`;
         $("table").prepend(newrow);
     }
 
@@ -96,14 +104,14 @@ $("thead tr th p i").click(function () {
         $("i").empty();
         $("i").text("keyboard_arrow_up");
         sort();
-        console.log($(this).hasClass("switch"));
+        // console.log($(this).hasClass("switch"));
     }
     if (!$(this).hasClass("switch"))
     {     
         $("i").empty();
         $("i").text("keyboard_arrow_down");
         sortbydesc();
-        console.log($(this).hasClass("switch"));   
+        // console.log($(this).hasClass("switch"));   
     }
     $(this).toggleClass("switch");
     
@@ -160,11 +168,11 @@ function sortbydesc() {
     $('table tbody').html(tbodyHtml);
 }
 
-function SendToMVC(event: any) {
-    console.log(event);
-    let newname: string = $("#name").text();
-    let newtype: string = $("#type").text();
-    let newisvisible = $("#isvisible").text();
-    console.log(`Новое название: ${newname}, Новый тип: ${newtype}, Новая видимость: ${newisvisible}`);
-    console.log("Данные отправлены на сервер по Ajax");
-}
+function SendToASPCore(currentrowid: any): void {
+    console.log(`id записи:${currentrowid}`);
+    console.log("Новое название:");
+    console.log($(`#${currentrowid}name`).text());
+    console.log("Новый тип:")
+    console.log($(`#${currentrowid}type`).text());
+    console.log("Данные отправлены в ASP.NET Core по Ajax");
+};
--- DinamicTable/assets/compiled/tablesort.js
+++ DinamicTable/assets/compiled/tablesort.js
@@ -1,57 +1,58 @@
 "use strict";
 var Row = /** @class */ (function () {
-    function Row(name, type, isvisible) {
+    function Row(id, name, type, isvisible) {
+        this.id = id;
         this.name = name;
         this.type = type;
         this.isvisble = isvisible;
     }
     return Row;
 }());
-var head = ["Имя", "Тип", "Видимость"];
-var data1 = new Row("Shannon Breitenberg", "Direct Tactics Producer", true);
-var data2 = new Row("Haley Schiller", "Central Applications Supervisor", true);
-var data3 = new Row("Sabrina Block II", "Legacy Functionality Officer", true);
-var data4 = new Row("Maybell Volkman", "Product Web Facilitator", false);
-var data5 = new Row("Zackery Paucek", "Regional Usability Liaison", true);
-var data6 = new Row("Dr. Krystal Wilkinson", "Principal Tactics Planner", false);
-var data7 = new Row("Chanelle McKenzie", "District Solutions Supervisor", true);
-var data8 = new Row("Billy Kunze II", "тип строка", true);
-var data9 = new Row("Eliezer Bailey", "Regional Accountability Executive", false);
-var data10 = new Row("Rickie Fay", "Corporate Configuration Orchestrator", true);
-var data11 = new Row("Kaycee Kub", "Chief Functionality Liaison", true);
-var data12 = new Row("Destinee Mueller PhD", "Dynamic Identity Facilitator", false);
-var data13 = new Row("Adeline Bins Sr.", "Regional Usability Associate", true);
-var data14 = new Row("Myrtie Kunze", "Dynamic Intranet Supervisor", false);
-var data15 = new Row("David Luettgen", "International Division Executive", true);
-var data16 = new Row("Ismael Nolan", "International Metrics Assistant", false);
-var data17 = new Row("Maegan Fay", "Principal Group Director", true);
-var data18 = new Row("Zander Adams", "Principal Division Assistant", true);
-var data19 = new Row("Onie Krajcik", "Chief Marketing Liaison", true);
-var data20 = new Row("Helmer Huels", "Lead Interactions Analyst", true);
-var data21 = new Row("Rhiannon Walker", "District Integration Developer", false);
-var data22 = new Row("Dianna Nolan", "Lead Quality Producer", true);
-var data23 = new Row("Garfield Upton", "National Quality Liaison", false);
-var data24 = new Row("Xavier Stamm", "Customer Configuration Administrator", true);
-var data25 = new Row("Gino Heller", "Product Tactics Supervisor", true);
-var data26 = new Row("Beulah Mitchell", "Investor Tactics Administrator", false);
-var data27 = new Row("Destany Johnston", "Forward Identity Director", true);
-var data28 = new Row("Kamron VonRueden", "Central Solutions Assistant", true);
-var data29 = new Row("Jaylen Nitzsche PhD", "Corporate Usability Administrator", false);
-var data30 = new Row("Turner Lemke", "Regional Data Developer", true);
-var data31 = new Row("Chris Schaefer", "Customer Security Officer", false);
-var data32 = new Row("Dillon Upton", "Regional Factors Associate", true);
-var data33 = new Row("Austen Marks", "Chief Response Consultant", false);
-var data34 = new Row("Kenton Cartwright", "Customer Intranet Architect", true);
+var head = ["Id", "Тип", "Видимость", "Активный"];
+var data1 = new Row(1, "Shannon Breitenberg", "Direct Tactics Producer", true);
+var data2 = new Row(2, "Haley Schiller", "Central Applications Supervisor", true);
+var data3 = new Row(3, "Sabrina Block II", "Legacy Functionality Officer", true);
+var data4 = new Row(4, "Maybell Volkman", "Product Web Facilitator", false);
+var data5 = new Row(5, "Zackery Paucek", "Regional Usability Liaison", true);
+var data6 = new Row(6, "Dr. Krystal Wilkinson", "Principal Tactics Planner", false);
+var data7 = new Row(7, "Chanelle McKenzie", "District Solutions Supervisor", true);
+var data8 = new Row(8, "Billy Kunze II", "Lead Quality Producer", true);
+var data9 = new Row(9, "Eliezer Bailey", "Regional Accountability Executive", false);
+var data10 = new Row(10, "Rickie Fay", "Corporate Configuration Orchestrator", true);
+var data11 = new Row(11, "Kaycee Kub", "Chief Functionality Liaison", true);
+var data12 = new Row(12, "Destinee Mueller PhD", "Dynamic Identity Facilitator", false);
+var data13 = new Row(13, "Adeline Bins Sr.", "Regional Usability Associate", true);
+var data14 = new Row(14, "Myrtie Kunze", "Dynamic Intranet Supervisor", false);
+var data15 = new Row(15, "David Luettgen", "International Division Executive", true);
+var data16 = new Row(16, "Ismael Nolan", "International Metrics Assistant", false);
+var data17 = new Row(17, "Maegan Fay", "Principal Group Director", true);
+var data18 = new Row(18, "Zander Adams", "Principal Division Assistant", true);
+var data19 = new Row(19, "Onie Krajcik", "Chief Marketing Liaison", true);
+var data20 = new Row(20, "Helmer Huels", "Lead Interactions Analyst", true);
+var data21 = new Row(21, "Rhiannon Walker", "District Integration Developer", false);
+var data22 = new Row(22, "Dianna Nolan", "Lead Quality Producer", true);
+var data23 = new Row(23, "Garfield Upton", "National Quality Liaison", false);
+var data24 = new Row(24, "Xavier Stamm", "Customer Configuration Administrator", true);
+var data25 = new Row(25, "Gino Heller", "Product Tactics Supervisor", true);
+var data26 = new Row(26, "Beulah Mitchell", "Investor Tactics Administrator", false);
+var data27 = new Row(27, "Destany Johnston", "Forward Identity Director", true);
+var data28 = new Row(28, "Kamron VonRueden", "Central Solutions Assistant", true);
+var data29 = new Row(29, "Jaylen Nitzsche PhD", "Corporate Usability Administrator", false);
+var data30 = new Row(30, "Turner Lemke", "Regional Data Developer", true);
+var data31 = new Row(31, "Chris Schaefer", "Customer Security Officer", false);
+var data32 = new Row(32, "Dillon Upton", "Regional Factors Associate", true);
+var data33 = new Row(33, "Austen Marks", "Chief Response Consultant", false);
+var data34 = new Row(34, "Kenton Cartwright", "Customer Intranet Architect", true);
 var data = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, data24, data25, data26, data27, data28, data29, data30, data31, data32, data33, data34];
 DrowTable(head, data);
 function DrowTable(head, data) {
-    var theader = "<thead scope=\"col\"><tr class=\"w3-red\"><th><p>" + head[1] + "<i id=\"head-name\" class=\"head-name w3-ext-cursor material-icons w3-ext-sort-icon\">unfold_more</i></p></th><th><p>" + head[1] + "</p></th><th>" + head[2] + "</th><th>\u0414\u0435\u0439\u0441\u0442\u0432\u0438\u044F</th></tr></thead>";
+    var theader = "<thead scope=\"col\"><tr class=\"w3-red\"><th><p>" + head[1] + "<i id=\"head-name\" class=\"head-name w3-ext-cursor material-icons w3-ext-sort-icon\">unfold_more</i></p></th><th><p>" + head[2] + "</p></th><th>" + head[3] + "</th><th>Action</th></tr></thead>";
     var tbody = "<tbody></tbody>";
     $("table").append(theader);
     $("table").append(tbody);
     for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
         var row = data_1[_i];
-        var newrow = "<tr><td><p contenteditable id=\"name\">" + row.name + "</p></td><td><p id=\"type\" contenteditable>" + row.type + "</p></td><td><select><option value=\"" + row.isvisble + "\">" + row.isvisble + "</option><option id=isvisible value=\"" + !row.isvisble + "\">" + !row.isvisble + "</option></select></td><td><a href=\"#\" onclick=\"SendToMVC(event)\" class=\"w3-ext-button-animate w3-button w3-small\">\u0421\u043E\u0445\u0440\u0430\u043D\u0438\u0442\u044C</a></td></tr>";
+        var newrow = "<tr><td><p contenteditable id=\"name\">" + row.name + "</p></td><td><p id=\"type\" contenteditable>" + row.type + "</p></td><td><select><option value=\"" + row.isvisble + "\">" + row.isvisble + "</option><option id=isvisible value=\"" + !row.isvisble + "\">" + !row.isvisble + "</option></select></td><td><a href=\"#\" onclick=\"SendToASPCore(event)\" class=\"w3-ext-button-animate w3-button w3-small\">\u0421\u043E\u0445\u0440\u0430\u043D\u0438\u0442\u044C</a></td></tr>";
         $("table").prepend(newrow);
     }
     $('table.w3-ext-paginated').each(function () {
@@ -77,21 +78,18 @@ function DrowTable(head, data) {
         $pager.insertAfter($table).find('span.page-number:first').addClass('w3-red');
     });
 }
-$("#my-button").on("click", function () {
-    // sort();
-});
 $("thead tr th p i").click(function () {
     if ($(this).hasClass("switch")) {
         $("i").empty();
         $("i").text("keyboard_arrow_up");
         sort();
-        console.log($(this).hasClass("switch"));
+        // console.log($(this).hasClass("switch"));
     }
     if (!$(this).hasClass("switch")) {
         $("i").empty();
         $("i").text("keyboard_arrow_down");
         sortbydesc();
-        console.log($(this).hasClass("switch"));
+        // console.log($(this).hasClass("switch"));   
     }
     $(this).toggleClass("switch");
 });
@@ -140,11 +138,12 @@ function sortbydesc() {
     }
     $('table tbody').html(tbodyHtml);
 }
-function SendToMVC(event) {
-    console.log(event);
-    var newname = $("#name").text();
-    var newtype = $("#type").text();
-    var newisvisible = $("#isvisible").text();
-    console.log("\u041D\u043E\u0432\u043E\u0435 \u043D\u0430\u0437\u0432\u0430\u043D\u0438\u0435: " + newname + ", \u041D\u043E\u0432\u044B\u0439 \u0442\u0438\u043F: " + newtype + ", \u041D\u043E\u0432\u0430\u044F \u0432\u0438\u0434\u0438\u043C\u043E\u0441\u0442\u044C: " + newisvisible);
-    console.log("Данные отправлены на сервер по Ajax");
+function SendToASPCore(currentrowid) {
+    console.log("id \u0437\u0430\u043F\u0438\u0441\u0438:" + currentrowid);
+    console.log("Новое название:");
+    console.log($("#" + currentrowid + "name").text());
+    console.log("Новый тип:");
+    console.log($("#" + currentrowid + "type").text());
+    console.log("Данные отправлены в ASP.NET Core по Ajax");
 }
+;
